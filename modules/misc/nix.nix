2e4a6eab711f74c48c60680ea33812ea84535839
dennkaii <githubdennkaii.q3i49@simplelogin.com>
2023-12-21 22:22:26 -0500

    addded anyrun substi

--- modules/misc/nix.nix
+++ modules/misc/nix.nix
@@ -30,6 +30,7 @@
       "https://cache.privatevoid.net"
        "https://nyx.cachix.org"
       "https://nix-gaming.cachix.org"
+      "https://anyrun.cachix.org"
       
     ];
     trusted-public-keys = [
@@ -38,6 +39,7 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "cache.privatevoid.net:SErQ8bvNWANeAvtsOESUwVYr2VJynfuc9JRwlzTTkVg="
       "nyx.cachix.org-1:xH6G0MO9PrpeGe7mHBtj1WbNzmnXr7jId2mCiq6hipE="
+    "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="  
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     ];
 
